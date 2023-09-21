9669c5b14a6b7b900e1dc59851602a3f7b853b78
paramaporn2003 <118731922+paramaporn2003@users.noreply.github.com>
2023-09-21 20:44:54 +0700

    add_background

--- app/courses/master-degree-course/page.tsx
+++ app/courses/master-degree-course/page.tsx
@@ -4,9 +4,12 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-      <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
-        <h2 className="text-4xl/[32px] font-bold ">Master's Degree Courses</h2>
-      </header> 
+      <section className="bg-gradient-to-r from-[#d1e0d8] to-[#76b9cd] py-12">
+        <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
+          <h2 className="text-4xl/[32px] font-bold ">Master's Degree Courses</h2>
+
+        </header> 
+      </section>
   )
 }
 
