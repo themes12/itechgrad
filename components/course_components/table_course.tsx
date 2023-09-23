e37d4b6d8d8cf9829cd8285aff8f0845f910c4f9
themes12 <phuttaamart@gmail.com>
2023-09-23 21:31:47 +0700

    Update table_course.tsx

--- components/course_components/table_course.tsx
+++ components/course_components/table_course.tsx
@@ -1,12 +1,11 @@
 "use client"
 import React from "react";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
-
-import "./Table-courses.css"
+import "./table_course.css"
 
 export const Table_courses = () => {
     return <div>
-            <Table removeWrapper>
+        <Table removeWrapper>
             <TableHeader>
                 <TableColumn className="table-header">Course code</TableColumn>
                 <TableColumn className="table-header">Course name</TableColumn>
@@ -14,27 +13,27 @@ export const Table_courses = () => {
             </TableHeader>
             <TableBody>
                 <TableRow key="1" className="table-row">
-                <TableCell className="table-cell-1" >204700</TableCell>
-                <TableCell className="table-cell-2">
-                    Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
-                </TableCell>
-                <TableCell className="table-cell-3">course description</TableCell>
+                    <TableCell className="table-cell-1" >204700</TableCell>
+                    <TableCell className="table-cell-2">
+                        Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
+                    </TableCell>
+                    <TableCell className="table-cell-3">course description</TableCell>
                 </TableRow>
                 <TableRow key="2" className="table-row">
-                <TableCell className="table-cell-1">204711</TableCell>
-                <TableCell className="table-cell-2">
-                    Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
-                </TableCell>
-                <TableCell className="table-cell-3">course description</TableCell>
+                    <TableCell className="table-cell-1">204711</TableCell>
+                    <TableCell className="table-cell-2">
+                        Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
+                    </TableCell>
+                    <TableCell className="table-cell-3">course description</TableCell>
                 </TableRow>
                 <TableRow key="3" className="table-row">
-                <TableCell className="table-cell-1">204745</TableCell>
-                <TableCell className="table-cell-2">
-                    Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
-                </TableCell>
-                <TableCell className="table-cell-3">course description</TableCell>
+                    <TableCell className="table-cell-1">204745</TableCell>
+                    <TableCell className="table-cell-2">
+                        Data Structure and Programming Languages <br /> โครงสร้างข้อมูลและภาษาโปรแกรม
+                    </TableCell>
+                    <TableCell className="table-cell-3">course description</TableCell>
                 </TableRow>
             </TableBody>
-            </Table>
+        </Table>
     </div>
 }
