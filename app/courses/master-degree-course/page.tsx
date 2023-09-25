f384a0649e773f7ed0d5cc5b2fb2a8c42d81aaac
Jiranthanin-Supakad <jiranthanin_su@cmu.ac.th>
2023-09-25 17:03:13 +0700

    update font size

--- app/courses/master-degree-course/page.tsx
+++ app/courses/master-degree-course/page.tsx
@@ -8,7 +8,7 @@ const page = (props: Props) => {
   return (
       <section className="bg-gradient-to-r from-[#d1e0d8] to-[#76b9cd] py-12">
         <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
-          <h2 className="text-4xl/[32px] font-bold ">Master's Degree Courses</h2>
+          <h2 className="text-[46px] font-bold ">Master's Degree Courses</h2>
           {/* <div className="pt-20">
             <div className="flex flex-col relative overflow-hidden height-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2
              shadow-small rounded-large transition-transform-background motion-reduce:transition-none border-none bg-white w-auto h-1/4 p-2" >
