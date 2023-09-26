3485e804da10eb4556561e4888de35189f52f620
phanukorn222 <118677350+phanukorn222@users.noreply.github.com>
2023-09-26 22:43:15 +0700

    fix acade

--- app/staff/academic/page.tsx
+++ app/staff/academic/page.tsx
@@ -6,46 +6,61 @@ import {CardStaff} from '@/components/card_staff/cardstaff';
 type Props = {}
 
 const page = (props: Props) => {
+  /*const posts = [
+    {
+        id: 1,
+        title: "",
+        body: "",
+        image_url: "",
+    },
+    {
+        id: 2,
+        title: "",
+        body: "",
+        image_url: "",
+    }];*/
   return (
+
     <div>
-    <section  className="bg-zinc-502">
-      <header className="flex z-40  h-auto items-center justify-center">
+      <Section>
+        <header className="flex z-40  h-auto items-center justify-center">
           <img className="relative" width={1520} height={789} src="/academic.jpg" />
-      </header>
-    </section>
-     <section  className="bg-zinc-502">
-      
-      <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
+        </header>
+      </Section>
+      <Section>
+
+        <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
           <div className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9">
             <h1 className="text-[64px] font-bold ">Academic Staff</h1>
-            
+
           </div>
-      </header>
-     </section>
-     <section  className="bg-zinc-502">
-      <header className="z-40  h-auto items-center justify-center">
-        <div className=" relative w-10/12 items-center">
-          <Divider className="my-4" />
+        </header>
+      </Section>
+      <Section>
+        <header className="pt-4 md:pt-8 lg:pt-10 md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-9 lg:px-9 relative">
+          <div className=" relative  items-center">
+            <div
+              style={{
+                width: '100%',
+                height: '1px',
+                backgroundColor: '#ccc',
+                margin: '50px 0',
+                display: 'block',
+              }} />
+          </div>
+        </header>
+      </Section>
+      <Section>
+        <div className="md:max-w-5xl lg:max-w-6xl xl:max-w-7xl mx-auto px-6 lg:px-9">
+          <div className="mb-24 relative mt-8">
+            <CardStaff></CardStaff>
+          </div>
         </div>
-      </header>
-     </section>
-     <section  className="bg-zinc-502">
-      <header className="z-40  h-auto items-center justify-center">
-        
-            <CardStaff></CardStaff>
-            
-      </header>
-     </section>
-     <section  className="bg-zinc-502">
-      <header className=" z-40  h-auto items-center justify-center">
-        
-            <CardStaff></CardStaff>
-            
-      </header>
-     </section>
+      </Section>
+
     </div>
-    
-  )
+
+  );
 }
 
 export default page
--- components/card_staff/cardstaff.tsx
+++ components/card_staff/cardstaff.tsx
@@ -1,66 +1,133 @@
 import React from "react";
 import {Spacer} from "@nextui-org/react";
 import {Card, CardHeader, CardBody, Image,Chip,Button} from "@nextui-org/react";
-import "./cardstaff.css"
 import {LinkNext} from './LinkNext';
 import {Mail} from './Mail';
 import {Phone} from './Phone';
+
+//import { acaStaff } from "@/types/academicstaffsec";
+//export const CardStaff = ({ nameThai, rank, nameEng, Tel, email, ResearchInterests, href, image}:acaStaff) =>
+
 export const CardStaff = () =>
-    <div>
-        <div className="retative container mx-auto 'md': '768px' px-60 py-8">
-            <div className="relative flex flex-row gap-4 ">
-                <div className="relative">
+    {
+        return <div>
+        <div className="relative container mx-auto py-8 px-8">
+            <div className="relative grid grid-cols-12 md:grid-cols-12">
+                <div className="block relative col-span-5 pl-40 md:cover">
                     <Image
-                        width={400}
+                      alt="MouckUp"
+                        className="rounded-xl object-cover object-center "
+                        src="https://drive.google.com/uc?export=view&id=11Q-WPNcCA_TcfZTCVsDBbDsfs7zzCcpq" 
                         
-                        alt="MouckUp"
-                        className="h-full w-auto object-cover rounded-xl "
-                        src="/MockUp1.jpg" />
+                        />
                 </div>
 
-                <div className="relative px-20 flex flex-col gap-6">
-                    <div className="relative">
-                        <h6 className="text-[24px] font-bold ">ผู้ช่วยศาสตราจารย์ ดร.วิจักษณ์ ศรีสัจจะเลิศวาจา</h6>
-                        <h6 className="text-[24px] font-bold ">Assistant Professor Dr.Wijak Srisujjalertwaja</h6>
-                    </div>
-                    <div className="relative">
-                        <p className="text-[18px]">รักษาการแทนหัวหน้าภาควิชาวิทยาการคอมพิวเตอร์</p>
-                        <p className="text-[18px]">ผู้ช่วยคณบดีฝ่ายเทคโนโลยีสารสนเทศ คณะวิทยาศาสตร์</p>
-                    </div>
-                    <div className="flex gap-2 relative">
-                        <Chip color="primary" className="text-black text-[14px] gap-8 bg-[#E7F6FD]">Recommender System</Chip>
-                        <Chip color="primary" className="text-black text-[14px] bg-[#E7F6FD]">Security management</Chip>
-                        <Chip color="primary" className="text-black text-[14px] bg-[#E7F6FD]">Webservice</Chip>
-                        <Chip color="primary" className="text-black text-[14px] bg-[#E7F6FD]">Ontology and semantic</Chip>
-                    </div>
-                    <div className="flex flex-col gap-3 relative">
-                        <div className="flex flex-row gap-4">
-                            <div>
-                                <Image
-                                    alt="Phone"
-                                    src="/Phone.svg" />
-                            </div>
-                            <div>053-943412 ต่อ 124</div>
+                <div className="relative col-span-7 px-20">
+                    <div className="flex flex-col gap-5">
+                        <div className="">
+                            <h6 className="text-[24px] font-bold ">ผู้ช่วยศาสตราจารย์ ดร.วิจักษณ์ ศรีสัจจะเลิศวาจา</h6>
+                            <h6 className="text-[24px] font-bold ">Assistant Professor Dr.Wijak Srisujjalertwaja</h6>
                         </div>
-                        <div className="flex flex-row gap-4">
-                            <div>
-                                <Image
-                                    alt="Mail"
-                                    src="/Mail.svg" />
-                            </div>
-                            <div>wijak.cscmu@gmail.com</div>
+                        <div className="">
+                            <p className="text-[18px]">รักษาการแทนหัวหน้าภาควิชาวิทยาการคอมพิวเตอร์</p>
+                            <p className="text-[18px]">ผู้ช่วยคณบดีฝ่ายเทคโนโลยีสารสนเทศ คณะวิทยาศาสตร์</p>
                         </div>
-                    </div>
-                    <div className="relative">
-                        <Button className="bg-[#054563] text-white text-[15px]" radius="full" endContent={<LinkNext />}>
-                            Personal Website
-                        </Button>
+                        <div className="relative gap-x-2 gap-y-2">
+                            <Chip color="primary" className="relative text-black text-[14px] gap-8 bg-[#E7F6FD]">Recommender System</Chip>
+                            <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Security management</Chip>
+                            <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Webservice</Chip>
+                            <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Ontology and semantic</Chip>
+                        </div>
+                        <div className="flex flex-col gap-3 ">
+                            <div className="flex flex-row gap-4">
+                                <div>
+                                    <Image
+                                        alt="Phone"
+                                        src="/Phone.svg" />
+                                </div>
+                                <div>053-943412 ต่อ 124</div>
+                            </div>
+                            <div className="flex flex-row  gap-4">
+                                <div>
+                                    <Image
+                                        alt="Mail"
+                                        src="/Mail.svg" />
+                                </div>
+                                <div>wijak.cscmu@gmail.com</div>
+                            </div>
+                        </div>
+                        <div className="">
+                            <Button className="bg-[#054563] text-white text-[15px]" radius="full" endContent={<LinkNext />}>
+                                Personal Website
+                            </Button>
+                        </div>
                     </div>
 
+                </div>
+            </div>
 
+
+        </div>
+    </div>;
+        
+        
+        /*<div>
+            <div className="relative container mx-auto py-8 px-8">
+                <div className="relative grid grid-cols-12 md:grid-cols-12">
+                    <div className="relative col-span-5 pl-40">
+                        <Image
+                          alt="MouckUp"
+                            className="rounded-xl object-cover object-center "
+                            src="https://drive.google.com/uc?export=view&id=11Q-WPNcCA_TcfZTCVsDBbDsfs7zzCcpq" 
+                            
+                            />
+                    </div>
+
+                    <div className=" col-span-7 px-20">
+                        <div className="flex flex-col gap-5">
+                            <div className="">
+                                <h6 className="text-[24px] font-bold ">ผู้ช่วยศาสตราจารย์ ดร.วิจักษณ์ ศรีสัจจะเลิศวาจา</h6>
+                                <h6 className="text-[24px] font-bold ">Assistant Professor Dr.Wijak Srisujjalertwaja</h6>
+                            </div>
+                            <div className="">
+                                <p className="text-[18px]">รักษาการแทนหัวหน้าภาควิชาวิทยาการคอมพิวเตอร์</p>
+                                <p className="text-[18px]">ผู้ช่วยคณบดีฝ่ายเทคโนโลยีสารสนเทศ คณะวิทยาศาสตร์</p>
+                            </div>
+                            <div className="relative gap-2 ">
+                                <Chip color="primary" className="relative text-black text-[14px] gap-8 bg-[#E7F6FD]">Recommender System</Chip>
+                                <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Security management</Chip>
+                                <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Webservice</Chip>
+                                <Chip color="primary" className="relative text-black text-[14px] bg-[#E7F6FD]">Ontology and semantic</Chip>
+                            </div>
+                            <div className="flex flex-col gap-3 ">
+                                <div className="flex flex-row gap-4">
+                                    <div>
+                                        <Image
+                                            alt="Phone"
+                                            src="/Phone.svg" />
+                                    </div>
+                                    <div>053-943412 ต่อ 124</div>
+                                </div>
+                                <div className="flex flex-row  gap-4">
+                                    <div>
+                                        <Image
+                                            alt="Mail"
+                                            src="/Mail.svg" />
+                                    </div>
+                                    <div>wijak.cscmu@gmail.com</div>
+                                </div>
+                            </div>
+                            <div className="">
+                                <Button className="bg-[#054563] text-white text-[15px]" radius="full" endContent={<LinkNext />}>
+                                    Personal Website
+                                </Button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
 
             </div>
-        </div>
-    </div>
+        </div>;*/
+    }
